b291b4fb4113bafca8917d845c6d876f3bacef42
wangmengfei <wangmengfei@bongmi.com>
2016-09-13 13:40:02 +0800

    function.scss
    新建function.scss，提取一些函数，方便日后调用

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,20 @@
 $(function(){
-    var indexVM = new Vue({
-        el: 'head',
-        data: {
-            title: '主页'
-        }
-    });
-    var navVM = new Vue({
-        el: '#navbar',
-        data: {
-            active:'Default',
-            navs:''
-        }
-    });
-    $.getJSON('../data/json/nav.json',function(json){
-        navVM.navs = json;
+    $("header").load("nav.html", function(){
+        var indexVM = new Vue({
+            el: 'head',
+            data: {
+                title: '主页'
+            }
+        });
+        var navVM = new Vue({
+            el: '#navbar',
+            data: {
+                active:'Default',
+                navs:''
+            }
+        });
+        $.getJSON('../data/json/nav.json',function(json){
+            navVM.navs = json;
+        });
     });
 });
--- static/js/personal.js
+++ static/js/personal.js
@@ -1,30 +1,31 @@
 $(function(){
-    var Nav = {
-        active:'Personal',
-        navs:''
-    };
-    var navVM = new Vue({
-        el: '#navbar',
-        data: Nav
+    $("header").load("nav.html", function(){
+        var Nav = {
+            active:'Personal',
+            navs:''
+        };
+        var navVM = new Vue({
+            el: '#navbar',
+            data: Nav
+        });
+        $.getJSON('../data/json/nav.json',function(json){
+            Nav.navs = json;
+        });
+        var PersonalList = {
+            lists:''
+        };
+        $.getJSON('../data/json/personal.json',function(json){
+            PersonalList.lists = json;
+        });
+        var personalListVM = new Vue({
+            el: '#content',
+            data: PersonalList
+        });
+        var indexVM = new Vue({
+            el: 'head',
+            data: {
+                title: '个人站点'
+            }
+        });
     });
-    $.getJSON('../data/json/nav.json',function(json){
-        Nav.navs = json;
-    });
-    var PersonalList = {
-        lists:''
-    };
-    $.getJSON('../data/json/personal.json',function(json){
-        PersonalList.lists = json;
-    });
-    var personalListVM = new Vue({
-        el: '#content',
-        data: PersonalList
-    });
-    var indexVM = new Vue({
-        el: 'head',
-        data: {
-            title: '个人站点'
-        }
-    });
-
 });
