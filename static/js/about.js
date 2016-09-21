ee151bb5f2cc76d54aab4ce567ef6b8d1e457f5f
wangmengfei <wangmengfei@bongmi.com>
2016-09-21 16:32:43 +0800

    jade
    将之前的html改成jade

--- static/js/about.js
+++ static/js/about.js
@@ -1,21 +1,13 @@
 $(function(){
-    $("header").load("nav.html", function(){
-        var indexVM = new Vue({
-            el: 'head',
-            data: {
-                title: '关于我'
-            }
-        });
-        var Nav = {
-            active:'About',
-            navs:''
-        };
-        var navVM = new Vue({
-            el: '#navbar',
-            data: Nav
-        });
-        $.getJSON('../data/json/nav.json',function(json){
-            Nav.navs = json;
-        });
+    var Nav = {
+        active:'About',
+        navs:''
+    };
+    var navVM = new Vue({
+        el: '#navbar',
+        data: Nav
+    });
+    $.getJSON('../data/json/nav.json',function(json){
+        Nav.navs = json;
     });
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,18 @@
 $(function(){
-    $("header").load("nav.html", function(){
-        var indexVM = new Vue({
-            el: 'head',
-            data: {
-                title: '主页'
-            }
-        });
-        var navVM = new Vue({
-            el: '#navbar',
-            data: {
-                active:'Default',
-                navs:''
-            }
-        });
-        $.getJSON('../data/json/nav.json',function(json){
-            navVM.navs = json;
-        });
+    var indexVM = new Vue({
+        el: 'head',
+        data: {
+            title: '主页'
+        }
+    });
+    var navVM = new Vue({
+        el: '#navbar',
+        data: {
+            active:'Default',
+            navs:''
+        }
+    });
+    $.getJSON('../data/json/nav.json',function(json){
+        navVM.navs = json;
     });
 });
